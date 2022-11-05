3ce1c5971473508e6b225464b5d60617813ac7a9
Gandar111 <101665553+Gandar111@users.noreply.github.com>
2022-11-05 16:18:16 +0100

    Array_Matrix Hauptdiagramm_Transponieren_summ
    Array_Matrix Hauptdiagramm_Transponieren_summ

--- Aufgabe_1.java
+++ Aufgabe_1.java
@@ -1,36 +1,99 @@
-package Eubungsbaltt_2;
+package Eubung_4;
 
-public class Aufgabbe_1 {
+import java.util.Arrays;
+import java.util.Iterator;
 
-	
+public class Aufgabe_1 {
 
-	
 	public static void main(String[] args) {
-		int[] arr = new int[5];
-		int i=0;
-		int i2=0;
-		int j=1;
-		int i3 = arr.length - 1;
-		while (i<arr.length) {
-			arr[i]=j;
-			i++;
-			j++;}
+		int summe_Hauptdiagonale=0;
+		int Summe_Matrix=0;
+		int spalte_Summ=0;
+		int [][] transprtierteArray= new int [4][4];
+		int NullWert=0;
 		
-		while ( i2 < arr.length ) {
+		
+		int [][]Array= {{-1,2,3,7},{3,4,5,0},{5,6,9,7},{5,-6,-9,-4}};
+		System.out.print("Der angegebene Matrix\n" );
+		for (int i = 0; i < 4; i++) {
+			summe_Hauptdiagonale+=Array[i][i];
+			{
+				for (int j = 0; j < 4; j++) {
+					
+					System.out.print(" "+Array[i][j]);
+					Summe_Matrix+=Array[i][j];
+				}System.out.print("\n");
+			}
 			
-			System.out.print(arr[i2] + " ");i2++;
 		}
-		System.out.println();
-		System.out.println("------------------- ") ;
-		while (i3>= 0) {
-			System.out.print(arr[i3] + " ");
-			i3--;
-			
-		}
-			
+System.out.print("-----------------------------------------------------------------------------------------------------------------------------------------------\n");
 
+		double Mittelwert=(double) Summe_Matrix/16;
+		System.out.print("Das arithmetische Mittel der Elemente der Matrix = "+Mittelwert);
+		System.out.print("\n");
+		System.out.print("Die Summe der Elemente der Hauptdiagonale = "+summe_Hauptdiagonale);
+		System.out.print("\n");
+System.out.print("-----------------------------------------------------------------------------------------------------------------------------------------------\n");
+
+		for (int i = 1, j=0; i < 17; i++) {
+			
+			spalte_Summ+=Array[i-1][j];
+			if(i==4){
+				int spalteNummer=j+1;
+				System.out.print("Das arithmetische Mittel der Elemente der Spalte "+spalteNummer+" = "+spalte_Summ);
+				System.out.print("\n");
+				spalte_Summ=0;
+				j++;
+				i=NullWert;
+				
+				if(j==4) {
+					break;
+				}}}
+System.out.print("-----------------------------------------------------------------------------------------------------------------------------------------------\n");
+System.out.println("Die transponierte Matrix");
+for (int i = 0; i < 4; i++) {
+	for (int j = 0; j < 4; j++) {
+		transprtierteArray[i][j]=Array[j][i];
+		System.out.print(" "+transprtierteArray[i][j]);
+		
+	}
+	System.out.print("\n");
+	
+}
+
+System.out.print("-----------------------------------------------------------------------------------------------------------------------------------------------\n");
+for (int i = 0; i < 4; i++) {
+	
+	for (int j = 0; j < 4; j++) {
+		if(Array[i][j]<0) {
+			
+			System.out.print(" "+0);
+		}
+		else {
+			System.out.print(" "+1);
+		}}
+	System.out.print("\n");
+}System.out.print("-----------------------------------------------------------------------------------------------------------------------------------------------\n");
+for (int i = 0; i < 4; i++) {
+	for (int j = 0; j < 4; j++) {
+		if(i==j) {
+			Array[i][j]=0;}
+		
+			System.out.print("  "+Array[i][j]);
+
+		
+
+	
+}System.out.print("\n");}
+}}
+				
+			
+		
+		
+		
+	
 
 
 	
 
-	}}
+
